2137a524bf90bcacbd0269d1e90b03928f2c5d3a
JoshuaToback <joshuatoback@gmail.com>
2023-03-02 20:35:30 -0500

    Added component templates

---
+++ client/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Login from "../components/login.js"
+import SignUp from "../components/signUp.js"
+
+
+export default function home() {
+    return (
+        <>
+        </>
+    )
+}
