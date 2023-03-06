ccc527c6cf57fde9d8055e380ced97a8bb5cf656
Mark Turner <mark.edward.turner@gmail.com>
2023-03-06 09:41:28 -0500

    updated mutations and schema for user and todo, still working on typedefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,25 +1,22 @@
 const { AuthenticationError } = require('@apollo/server');
-const { User, Thought } = require('../models');
+const { User, Todo } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    users: async () => {
-      return User.find().populate('thoughts');
+      user: async (parent, { username }) => {
+      return User.findOne({ username }).populate('todos');
     },
-    user: async (parent, { username }) => {
-      return User.findOne({ username }).populate('thoughts');
-    },
-    thoughts: async (parent, { username }) => {
+    todos: async (parent, { username }) => {
       const params = username ? { username } : {};
-      return Thought.find(params).sort({ createdAt: -1 });
+      return Todo.find(params).sort({ createdAt: -1 });
     },
-    thought: async (parent, { thoughtId }) => {
-      return Thought.findOne({ _id: thoughtId });
+    todo: async (parent, { thoughtId }) => {
+      return Todo.findOne({ _id: todoId });
     },
     me: async (parent, args, context) => {
       if (context.user) {
-        return User.findOne({ _id: context.user._id }).populate('thoughts');
+        return User.findOne({ _id: context.user._id }).populate('todos');
       }
       throw new AuthenticationError('You need to be logged in!');
     },
@@ -48,40 +45,24 @@ const resolvers = {
 
       return { token, user };
     },
-    addThought: async (parent, { thoughtText }, context) => {
+    addToDo: async (parent, { todo }, context) => {
       if (context.user) {
         const thought = await Thought.create({
-          thoughtText,
-          thoughtAuthor: context.user.username,
+          toDoContent,
+          toDoAuthor: context.user.username,
         });
 
         await User.findOneAndUpdate(
           { _id: context.user._id },
-          { $addToSet: { thoughts: thought._id } }
+          { $addToSet: { todos: todo._id } }
         );
 
         return thought;
       }
       throw new AuthenticationError('You need to be logged in!');
     },
-    addComment: async (parent, { thoughtId, commentText }, context) => {
-      if (context.user) {
-        return Thought.findOneAndUpdate(
-          { _id: thoughtId },
-          {
-            $addToSet: {
-              comments: { commentText, commentAuthor: context.user.username },
-            },
-          },
-          {
-            new: true,
-            runValidators: true,
-          }
-        );
-      }
-      throw new AuthenticationError('You need to be logged in!');
-    },
-    removeThought: async (parent, { thoughtId }, context) => {
+ 
+    deleteToDo: async (parent, { todoId }, context) => {
       if (context.user) {
         const thought = await Thought.findOneAndDelete({
           _id: thoughtId,
@@ -97,23 +78,7 @@ const resolvers = {
       }
       throw new AuthenticationError('You need to be logged in!');
     },
-    removeComment: async (parent, { thoughtId, commentId }, context) => {
-      if (context.user) {
-        return Thought.findOneAndUpdate(
-          { _id: thoughtId },
-          {
-            $pull: {
-              comments: {
-                _id: commentId,
-                commentAuthor: context.user.username,
-              },
-            },
-          },
-          { new: true }
-        );
-      }
-      throw new AuthenticationError('You need to be logged in!');
-    },
+ 
   },
 };
 
